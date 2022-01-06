fe10d7d7ace99704209761b66c754c00f3df9606
Tassol <carol.o.borin@gmail.com>
2022-01-06 14:45:03 +0100

    ready to deploy

--- app.js
+++ app.js
@@ -17,12 +17,13 @@ const helmet = require('helmet');
 
 const mongoSanitize = require('express-mongo-sanitize');
 
-
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
+const MongoStore = require('connect-mongo');
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect(dbUrl, {
   useUnifiedTopology: true,
 });
 
@@ -44,10 +45,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize({
   replaceWith: '_'
 }))
+const secret = process.env.SECRET || 'hehehe';
+
+const store = MongoStore.create ({
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 60 * 60,
+  crypto:{
+    secret,
+  }
+});
+
+store.on("error",function (e){
+  console.log("Session store error", e)
+})
 
 const sessionConfig = {
+  store,
   name: 'session',
-  secret: 'hehehehehe',
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
